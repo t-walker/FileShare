c5bb60ae2a602799ffc6d52bc68795d845e3a317
Tyler Walker <twalker@eecs.wsu.edu>
2014-11-07 10:31:13 -0800

    Additional hashing functions created

--- hashing.py
+++ hashing.py
@@ -4,6 +4,10 @@ from hashlib import md5
 from os import listdir
 
 def file_2_hash(file):
+    """
+    @type file: file
+    :rtype: md5 object
+    """
     return md5(open(file).read()).hexdigest()
 
 def directory_2_hash(path):
@@ -11,10 +15,12 @@ def directory_2_hash(path):
     @type path: string
     :rtype: list
     """
-
     hashed_directory = []
 
     for file in listdir(path):
         hashed_directory.append(file_2_hash(file))
 
     return hashed_directory
+
+def send_hash(file_hash):
+    return 0
