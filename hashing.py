4f4f7f03bdfceb2869cdfdf4fae7899fd95abe78
Tyler Walker <twalker@eecs.wsu.edu>
2014-11-08 16:41:10 -0800

    Updated main
    file_2_hash : updated with functionality
    directory_2_hash : updated with functionality

--- hashing.py
+++ hashing.py
@@ -3,28 +3,30 @@ __author__ = 'Tyler'
 from hashlib import md5
 from os import listdir
 
+
 def file_2_hash(file):
     """
     @type file: file
     :rtype: md5 object
     """
-    return md5(open(file).read()).hexdigest()
+    file_hash = md5()
+    file_hash.update(open(file, 'rb').read())
+    file_hash.update(file.encode('UTF-8'))
+
+    return file_hash.hexdigest()
+
 
 def directory_2_hash(path):
     """
+    :param path:
     @type path: string
     :rtype: list
     """
     hashed_directory = []
 
     for file in listdir(path):
-        hashed_directory.append(file_2_hash(file))
-
+        try:
+            hashed_directory.append(file_2_hash(file))
+        except PermissionError:
+            print("[-] ERROR: Cannot access:", file)
     return hashed_directory
-
-def send_hash(file_hash):
-    return 0
-
-def check_hash(hash_1, hash_2):
-    return 0
-    
---
+++ main.py
@@ -0,0 +1,22 @@
+__author__ = 'Tyler'
+
+import os
+from hashing import file_2_hash, directory_2_hash
+
+
+def main():
+    file_1 = "test.txt"
+    file_2 = "test_1.txt"
+    directory_1 = os.getcwd()
+    print("File 1:", file_2_hash(file_1))
+    print("File 2:", file_2_hash(file_2))
+
+    directory_hash = directory_2_hash(directory_1)
+
+    for file in directory_hash:
+        print(file)
+
+    return 0
+
+if __name__ == "__main__":
+    main()
