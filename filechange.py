8abe74c0d3717a65f5d5b995a2c577e1c06a3c3a
Tyler Walker <twalker@eecs.wsu.edu>
2014-11-15 06:00:54 +0000

    Files aren't updating...

--- filechange.py
+++ filechange.py
@@ -4,4 +4,7 @@ def file_hash_DICT(file_names, file_hashes):
         dict_of_files[file_names[i]] = file_hashes[i]
     return dict_of_files
     
-    
+def assemble_files(directory):
+    file_names, file_hashes = directory_2_hash(directory)
+    hashed_dir = file_hash_DICT(file_names, file_hashes)
+    return (file_names, hashed_dir)
